c0db65abdb51287a0b26823af8f1fc8069579c84
Tian Wu <wutian@maxtropy.com>
2021-05-15 11:54:52 +0800

    54, 排序数组中查找目标的第一个和最后一个下标

--- src/main/java/com/metalgeek/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/metalgeek/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,44 +1,44 @@
-//给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。 
+//给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
 //
-// 如果数组中不存在目标值 target，返回 [-1, -1]。 
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
 //
-// 进阶： 
+// 进阶：
 //
-// 
-// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？ 
-// 
 //
-// 
+// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
 //
-// 示例 1： 
 //
-// 
+//
+//
+// 示例 1：
+//
+//
 //输入：nums = [5,7,7,8,8,10], target = 8
-//输出：[3,4] 
+//输出：[3,4]
 //
-// 示例 2： 
+// 示例 2：
 //
-// 
+//
 //输入：nums = [5,7,7,8,8,10], target = 6
-//输出：[-1,-1] 
+//输出：[-1,-1]
 //
-// 示例 3： 
+// 示例 3：
 //
-// 
+//
 //输入：nums = [], target = 0
-//输出：[-1,-1] 
+//输出：[-1,-1]
 //
-// 
 //
-// 提示： 
 //
-// 
-// 0 <= nums.length <= 105 
-// -109 <= nums[i] <= 109 
-// nums 是一个非递减数组 
-// -109 <= target <= 109 
-// 
-// Related Topics 数组 二分查找 
+// 提示：
+//
+//
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums 是一个非递减数组
+// -109 <= target <= 109
+//
+// Related Topics 数组 二分查找
 // 👍 1002 👎 0
 
 
@@ -51,39 +51,50 @@ public class FindFirstAndLastPositionOfElementInSortedArray{
        Solution solution = new FindFirstAndLastPositionOfElementInSortedArray().new Solution();
 
        int[] test1 = new int[]{5,7,7,8,8,10}; // 8
-       int[] test2 = new int[]{5,7,7,8,8,10}; // 6
+//       int[] test2 = new int[]{5,7,7,8,8,10}; // 6
+      int[] test3 = new int[]{1}; // 6
        long t0 = System.currentTimeMillis();
        // Call solution here
 //      System.out.println(Arrays.asList(solution.searchRange(test1, 8)));
-      System.out.println(Arrays.asList(solution.searchRange(test2, 6)));
-       
+//      System.out.println(Arrays.asList(solution.searchRange(test2, 6)));
+      System.out.println(Arrays.asList(solution.searchRange(test3, 1)));
+
        long t1 = System.currentTimeMillis();
        System.out.println("time used " + (t1-t0));
   }
   //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int i = 0, j = nums.length - 1, mid =-1, num;
-        while(i <= j) {
-            mid = (i + j) >> 1;
-            num = nums[mid];
-            if(num > target) {
-                j = mid -1;
-            }else if(num < target) {
-                i = mid;
+        if(nums.length ==0) {
+            return new int[]{-1, -1};
+        }
+
+        int l = 0, r = nums.length - 1, mid;
+        int left;
+        while(l < r) {
+            mid = l + (r - l) /2;
+            if(nums[mid] >= target) {
+                r = mid;
             }else {
-                break;
+                l = mid +1;
             }
         }
-        i = mid;
-        j = mid;
-        while(nums[i] == target) {
-            i--;
+        left = l;
+        if (nums[left] != target) {
+            return new int[]{-1, -1};
         }
-        while(nums[j] == target) {
-            j++;
+
+        l = 0; r = nums.length - 1;
+        while(l < r) {
+            mid = l + (r - l +1) /2;
+            if(nums[mid] <= target) {
+                l = mid;
+            }else {
+                r = mid - 1;
+            }
         }
-        return new int[]{i+1, j-1};
+
+        return new int[]{left, r};
 
     }
 }
