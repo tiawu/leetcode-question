c3183e9c39c8cdad35f3b3915f96e78c20ec9c3e
wutian@maxtropy.com <wutian@maxtropy.com>
2021-05-23 01:28:23 +0800

    92, 二叉树节点的最近公共祖先

---
+++ src/main/java/com/metalgeek/leetcode/editor/cn/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,97 @@
+//给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。” 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//输出：3
+//解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//输出：5
+//解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1,2], p = 1, q = 2
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [2, 105] 内。 
+// -109 <= Node.val <= 109 
+// 所有 Node.val 互不相同 。 
+// p != q 
+// p 和 q 均存在于给定的二叉树中。 
+// 
+// Related Topics 树 
+// 👍 1121 👎 0
+
+
+package com.metalgeek.leetcode.editor.cn;
+
+import com.metalgeek.leetcode.TreeNode;
+
+public class LowestCommonAncestorOfABinaryTree{
+  public static void main(String[] args) {
+       Solution solution = new LowestCommonAncestorOfABinaryTree().new Solution();
+       
+       long t0 = System.currentTimeMillis();
+       // Call solution here
+       System.out.println("");
+       
+       long t1 = System.currentTimeMillis();
+       System.out.println("time used " + (t1-t0));
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode res;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        res = null;
+        dfs(root, p, q);
+        return res;
+    }
+    // DFS递归
+    public boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return false;
+        }
+
+        boolean lc = dfs(root.left, p, q);
+        boolean rc = dfs(root.right, p, q);
+
+        boolean ret = (lc && rc) || ((lc || rc) && (root.val == p.val || root.val == q.val));
+        if(ret) {
+            res = root;
+        }
+        // 左右子树或本节点含有p/q之一即可为true, 递归回退上层时用于判断ret表达式
+        return lc || rc || root.val == p.val || root.val == q.val;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
