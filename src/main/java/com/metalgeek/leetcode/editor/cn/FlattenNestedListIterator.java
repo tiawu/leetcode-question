3c52101f0e92a8ab89297259aab5cf07574a1498
Tian Wu <wutian@maxtropy.com>
2021-05-27 19:11:15 +0800

    110, 扁平化嵌套列表迭代器

---
+++ src/main/java/com/metalgeek/leetcode/editor/cn/FlattenNestedListIterator.java
@@ -0,0 +1,113 @@
+//给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+//
+// 列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+//
+//
+//
+// 示例 1:
+//
+// 输入: [[1,1],2,[1,1]]
+//输出: [1,1,2,1,1]
+//解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+//
+// 示例 2:
+//
+// 输入: [1,[4,[6]]]
+//输出: [1,4,6]
+//解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+//
+// Related Topics 栈 设计
+// 👍 338 👎 0
+
+
+package com.metalgeek.leetcode.editor.cn;
+
+import java.util.*;
+
+public class FlattenNestedListIterator{
+    public static void main(String[] args) {
+         Solution solution = new FlattenNestedListIterator().new Solution();
+
+         long t0 = System.currentTimeMillis();
+         // Call solution here
+         System.out.println("");
+
+         long t1 = System.currentTimeMillis();
+         System.out.println("time used " + (t1-t0));
+    }
+
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+public interface NestedInteger {
+
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return empty list if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+}
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class NestedIterator implements Iterator<Integer> {
+    Deque<Iterator<NestedInteger>> stack;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new LinkedList<>();
+        stack.push(nestedList.iterator());
+    }
+
+    @Override
+    public Integer next() {
+        return stack.peek().next().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while(!stack.isEmpty()) {
+            if(!stack.peek().hasNext()) {
+                stack.pop();
+                continue;
+            }
+            NestedInteger next = stack.peek().next();
+            if(next.isInteger()) {
+                stack.push(Collections.singletonList(next).iterator());
+                return true;
+            }else {
+                stack.push(next.getList().iterator());
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
