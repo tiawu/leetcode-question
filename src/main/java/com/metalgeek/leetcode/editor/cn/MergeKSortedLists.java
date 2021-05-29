89eafb8bddcca3382fd90d28243a9e5e7264b456
Tian Wu <wutian@maxtropy.com>
2021-05-29 12:04:34 +0800

    wip 合并K个升序链表

---
+++ src/main/java/com/metalgeek/leetcode/editor/cn/MergeKSortedLists.java
@@ -0,0 +1,119 @@
+//给你一个链表数组，每个链表都已经按升序排列。
+//
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+//
+//
+//
+// 示例 1：
+//
+// 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+//输出：[1,1,2,3,4,4,5,6]
+//解释：链表数组如下：
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//将它们合并到一个有序链表中得到。
+//1->1->2->3->4->4->5->6
+//
+//
+// 示例 2：
+//
+// 输入：lists = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+// 输入：lists = [[]]
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// k == lists.length
+// 0 <= k <= 10^4
+// 0 <= lists[i].length <= 500
+// -10^4 <= lists[i][j] <= 10^4
+// lists[i] 按 升序 排列
+// lists[i].length 的总和不超过 10^4
+//
+// Related Topics 堆 链表 分治算法
+// 👍 1321 👎 0
+
+
+package com.metalgeek.leetcode.editor.cn;
+
+import com.metalgeek.leetcode.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeKSortedLists{
+    public static void main(String[] args) {
+         Solution solution = new MergeKSortedLists().new Solution();
+
+         long t0 = System.currentTimeMillis();
+         // Call solution here
+         System.out.println("");
+
+         long t1 = System.currentTimeMillis();
+         System.out.println("time used " + (t1-t0));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return recurseMerge(lists, 0, lists.length-1);
+
+    }
+
+    public ListNode recurseMerge(ListNode [] list , int i, int j) {
+        if(i > j) {
+            return null;
+        }
+        if(i == j) {
+            return list[i];
+        }
+
+        int mid = (i + j) /2;
+        return merge(recurseMerge(list, i, mid), recurseMerge(list, mid+1, j));
+    }
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+        if(list1 == null || list2 == null) {
+            return list1 == null ? list2 : list1;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        ListNode pre = dummyHead;
+        while(list1 != null || list2 != null) {
+            if(list1.val < list2.val) {
+                pre.next = list1;
+                list1 = list1.next;
+            }else {
+                pre.next = list2;
+                list2 = list2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = list1 == null ? list2 : list1;
+        return dummyHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
